53c367296719c5680902f690c90d197853dd3f29
jcdcdev <james@jcdc.dev>
2023-10-06 15:52:23 +0100

    fix: StoreUpdateFrequency default value

--- src/jcdcdev.Eco.Core/Models/CoreConfig.cs
+++ src/jcdcdev.Eco.Core/Models/CoreConfig.cs
@@ -8,5 +8,6 @@ public class CoreConfig
     [Description("The rate in seconds (5-120) at which store data is updated. Default: 10s")]
     [Category("Performance")]
     [Range(5, 120)]
+    [DefaultValue(10)]
     public uint StoreUpdateFrequency { get; set; } = 10;
 }
