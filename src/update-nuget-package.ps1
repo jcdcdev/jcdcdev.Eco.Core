b5827644019c12e45fe0e45327685198fb72e24d
jcdcdev <james@jcdc.dev>
2024-06-23 20:40:44 +0100

    +changelog-ignore: enable pr creation

--- src/update-nuget-package.ps1
+++ src/update-nuget-package.ps1
@@ -1,72 +1,66 @@
 param(
-[Parameter(Mandatory=$true)]
-[string]$projectFilePath,
-
-[Parameter(Mandatory=$true)]
-[string]$packageId
+    [Parameter(Mandatory = $true)]
+    [string]$projectFilePath,
+    [Parameter(Mandatory = $true)]
+    [string]$packageId,
+    [Parameter(Mandatory = $false)]
+    [switch]$checkOnly
 )
 
 $pattern = [regex]"((PackageReference)+.(Include=""$packageId"").+(Version=""(.+)\""))"
 
 $content = Get-Content -Path $projectFilePath -Raw
-$matches = $pattern.Matches($content)
+$results = $pattern.Matches($content)
 
-if ($matches.Count -eq 0)
-{
-    Write-Host "No $packageId package reference found in the project file."
-    return;
+if ($results.Count -eq 0) {
+    Write-Warning "⚠️ No $packageId package reference found in the project file."
+    exit 0;
 }
 
-if ($matches.Count -gt 1)
-{
-    Write-Host "Multiple $packageId package references found in the project file."
-    return;
+if ($results.Count -gt 1) {
+    Write-Warning "⚠️ Multiple $packageId package references found in the project file."
+    exit 1;
 }
 
-$match = $matches[0]
+$match = $results[0]
 
 $version = $match.Groups[5].Value
-Write-Host "Current Version`n`n$version`n"
+Write-Host "🔃 Current Version`n`n$version`n"
 
-try
-{
-    $latestVersionResponse = Invoke-RestMethod "https://api.nuget.org/v3-flatcontainer/$($packageId.ToLowerInvariant() )/index.json"
+try {
+    $latestVersionResponse = Invoke-RestMethod "https://api.nuget.org/v3-flatcontainer/$($packageId.ToLowerInvariant())/index.json"
 }
-catch
-{
-    Write-Warning "Failed to retrieve the latest version of $packageId."
-    return;
+catch {
+    Write-Warning "⚠️  Failed to retrieve the latest version of $packageId."
+    exit 1;
 }
 
 $index = $latestVersionResponse.versions.IndexOf($version)
-if ($index -eq -1)
-{
-    Write-Host "$version is not found in the list of versions on NuGet."
-    return;
+if ($index -eq -1) {
+    Write-Warning "⚠️  $version is not found in the list of versions on NuGet."
+    exit 1;
 }
 
-if ($index -eq $latestVersionResponse.versions.Count - 1)
-{
-    Write-Host "$packageId is up-to-date."
-    return;
+if ($index -eq $latestVersionResponse.versions.Count - 1) {
+    Write-Host "✅  $packageId is up-to-date."
+    exit 0;
 }
 
 $updatedVersion = $latestVersionResponse.versions[$index + 1]
 
-Write-Host "Next Version`n`n$updatedVersion`n"
+Write-Host "⬆️  Next Version`n`n$updatedVersion`n"
 
-$newContent = $match.Value -replace "(Version=`"(.*?)`")", "Version=""$updatedVersion"" "
-Write-Host $newContent
-
-$updatedContent = $content.Replace($match.Value, $newContent)
-Write-Verbose $updatedContent
-
-$updatedContent | Set-Content -Path $projectFilePath -NoNewline -Encoding Default
+if (-not $checkOnly) {
+    $newContent = $match.Value -replace "(Version=`"(.*?)`")", "Version=""$updatedVersion"" "
+    Write-Verbose "New csproj line: $newContent"
+    $updatedContent = $content.Replace($match.Value, $newContent)
+    $updatedContent | Set-Content -Path $projectFilePath -NoNewline -Encoding Default
+}
 
 return @{
-    InitialVersion = $version
-    UpdatedVersion = $updatedVersion
-    Updated = $version -ne $updatedVersion
+    SourceVersion = $version
+    TargetVersion = $updatedVersion
+    Updated       = $version -ne $updatedVersion
 }
 
 
